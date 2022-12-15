d421ba6041a7f5577f6888f3416a421ee4a8e2fb
kuninaa19 <minchan0527@naver.com>
2022-12-16 02:44:12 +0900

    feat: 게임 진행을 위한 메인 클래스 추가

---
+++ src/main/java/racingcar/RacingCarApplication.java
@@ -0,0 +1,8 @@
+package racingcar;
+
+public class RacingCarApplication {
+    public static void main(String[] args) {
+        FlowController flowController = new FlowController();
+        flowController.run();
+    }
+}
