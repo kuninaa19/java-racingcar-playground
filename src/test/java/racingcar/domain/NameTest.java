d4ed2cb3e5216b9bcc0dca71251575abfec28eea
kuninaa19 <minchan0527@naver.com>
2022-12-16 23:47:19 +0900

    test(name): 네임 테스트 분리
    이름 글자 수 허용, 이름 글자 수 에러 테스트 분리

--- src/test/java/racingcar/domain/NameTest.java
+++ src/test/java/racingcar/domain/NameTest.java
@@ -1,19 +1,21 @@
 package racingcar.domain;
 
 import org.junit.jupiter.api.Test;
+import racingcar.messages.ErrorMessages;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class NameTest {
     @Test
-    void 자동차_이름_선정_5자이하() {
-        Name SuccessCase = new Name("crong");
+    void 이름_허용_길이_5자이하() {
+        assertThat(new Name("crong")).isEqualTo(new Name("crong"));
+    }
 
-        assertThat(SuccessCase.getName()).isEqualTo("crong");
-
+    @Test
+    void 이름_선정_길이_에러() {
         assertThatThrownBy(() -> {
             Name FailCase = new Name("crong+");
-        }).isInstanceOf(IllegalArgumentException.class).hasMessage("자동차 이름은 5자 이하만 허용합니다.");
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage(ErrorMessages.CAR_NAME_SIZE_EXCEPTION);
     }
 }
