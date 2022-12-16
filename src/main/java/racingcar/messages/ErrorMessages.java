69edf8cabc5447a3be55fd96b2e115024fb5d7fe
kuninaa19 <minchan0527@naver.com>
2022-12-17 00:18:59 +0900

    style: 사용하지않는 의존 삭제

--- src/main/java/racingcar/messages/ErrorMessages.java
+++ src/main/java/racingcar/messages/ErrorMessages.java
@@ -1,7 +1,5 @@
 package racingcar.messages;
 
-import racingcar.constants.CarConstants;
-
 public class ErrorMessages {
     public static final String CAR_NAME_SIZE_EXCEPTION = "[ERROR] 자동차 이름은 5자 이하만 허용합니다.";
     public static final String MOVE_COUNT_TYPE_EXCEPTION = "[ERROR] 숫자를 입력해주세요.";
